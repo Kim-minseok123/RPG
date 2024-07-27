25bc67202692d2694a78a515a1c7a533a04b663a
Bedford <83286384+Kim-minseok123@users.noreply.github.com>
2024-07-27 14:25:02 +0900

    fix : mesh 삭제 및 3d 사운드 수정

--- Server/AccountServer/obj/Debug/netcoreapp3.1/AccountServer.AssemblyInfo.cs
+++ Server/AccountServer/obj/Debug/netcoreapp3.1/AccountServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9414afc2453d5aba14dae5a1c86affe9df580743")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/Server/obj/Debug/netcoreapp3.1/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/netcoreapp3.1/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9414afc2453d5aba14dae5a1c86affe9df580743")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
